90f06200d838b02a23e66ba79bda89487ffab9a5
kknd9819 <kknd9819@qq.com>
2018-01-28 19:55:15 +0800

    1

--- src/main/java/com/zz/shengyuan/SignApplication.java
+++ src/main/java/com/zz/shengyuan/SignApplication.java
@@ -2,9 +2,15 @@ package com.zz.shengyuan;
 
 import com.zz.shengyuan.storage.StorageProperties;
 import com.zz.shengyuan.storage.StorageService;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
@@ -32,6 +38,36 @@ public class SignApplication {
 			//	storageService.deleteAll();
 			storageService.init();
 		};
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+			@Override
+			protected void postProcessContext(Context context) {
+
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(10011);
+		connector.setSecure(false);
+		connector.setRedirectPort(10012);
+		return connector;
 	}
 
 
